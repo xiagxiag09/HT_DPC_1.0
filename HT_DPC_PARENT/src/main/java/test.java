249b72da906a96502cf9fc82089bcbb1a46528fc
zhouweilong <xiag_xiag09@163.com>
2016-05-18 14:47:12 +0800

    工程文件

---
+++ HT_DPC_PARENT/src/main/java/test.java
@@ -0,0 +1,34 @@
+/**
+ *
+ */
+package com.huitu.test.base;
+
+import junit.framework.TestCase;
+
+/**
+ * @author admin
+ */
+public class BaseTests extends TestCase {
+
+    /**
+     * @param name
+     */
+    public BaseTests(String name) {
+        super(name);
+    }
+
+    /* (non-Javadoc)
+     * @see junit.framework.TestCase#setUp()
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    /* (non-Javadoc)
+     * @see junit.framework.TestCase#tearDown()
+     */
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+}
